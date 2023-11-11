0e89ee96af9a08292039f88edbda90e08d96d0d1
Emirfahimi0 <emirfahimi0@gmail.com>
2023-11-12 00:40:07 +0800

    update components

--- src/components/ShoppingCartCanvas.tsx
+++ src/components/ShoppingCartCanvas.tsx
@@ -1,4 +1,4 @@
-import React, { FunctionComponent } from "react";
+import { FunctionComponent } from "react";
 import { Offcanvas, Stack } from "react-bootstrap";
 import { CartItemComponent } from ".";
 import { formatCurrencies } from "../utils";
