dcefb612ba095690495a3ee63b10df2dff777a05
Emirfahimi0 <emirfahimi0@gmail.com>
2023-11-11 15:42:51 +0800

    update store page

--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -1,5 +1,21 @@
-import React from "react";
+import { Col, Row } from "react-bootstrap";
+import storeItems from "../data/items.json";
+import { FunctionComponent } from "react";
+import { StoreItems } from "../components";
 
-export const Store = () => {
-  return <div>Store</div>;
+export const Store: FunctionComponent = () => {
+  return (
+    <div>
+      <h1>Store</h1>
+      <Row md={5} xs={1} lg={3} className="g-3">
+        {storeItems.map((eachItems: IStoreItems) => {
+          return (
+            <Col key={eachItems.id}>
+              <StoreItems quantity={1} {...eachItems} />
+            </Col>
+          );
+        })}
+      </Row>
+    </div>
+  );
 };
